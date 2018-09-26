912c267c906af51a4c1357e75c0d5b5f2f46c9f4
Elías Biscaia <elu1996biscaia@gmail.com>
2018-09-25 23:21:30 -0300

    Update Main.java

--- src/formandoequipos/Main.java
+++ src/formandoequipos/Main.java
@@ -5,22 +5,10 @@ import java.io.IOException;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		Mejorequipo e1 = new Mejorequipo("00_enunciado");
+		Mejorequipo e1 = new Mejorequipo("equipo");
 		e1.buscarmejorequipo();
 		System.out.println(e1);
-		e1.escribir("00_enunciado");
-		Mejorequipo e2 = new Mejorequipo("01_gana_cantidad_colaboradores");
-		e2.buscarmejorequipo();
-		System.out.println(e2);
-		e2.escribir("01_gana_cantidad_colaboradores");
-		Mejorequipo e3 = new Mejorequipo("02_gana_cantidad_respuestas");
-		e3.buscarmejorequipo();
-		System.out.println(e3);
-		e3.escribir("02_gana_cantidad_respuestas");
-		Mejorequipo e4 = new Mejorequipo("03_fatiga");
-		e4.buscarmejorequipo();
-		System.out.println(e4);
-		e4.escribir("03_fatiga");
+		e1.escribir("equipo");
 	}
 
 }
